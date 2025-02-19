464417c496461f117b72f38dc6aec5153867c995
Avaray <1459553+Avaray@users.noreply.github.com>
2025-02-19 14:05:54 +0100

    added code_93

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import ean13 from "./barcode-types/ean_13.ts";
 import code128 from "./barcode-types/code_128.ts";
+import code93 from "./barcode-types/code_93.ts";
 import code39 from "./barcode-types/code_39.ts";
 
-type CodeTypes = "upc_a" | "ean_13" | "code_128" | "code_39";
+type CodeTypes = "upc_a" | "ean_13" | "code_128" | "code_93" | "code_39";
 
 export default class TsBarcodeGenerator {
   public static generate(code: string, type: CodeTypes): string {
@@ -12,6 +13,8 @@ export default class TsBarcodeGenerator {
         return ean13.generate(code);
       case "code_128":
         return code128.generate(code);
+      case "code_93":
+        return code93.generate(code);
       case "code_39":
         return code39.generate(code);
       default:
